bfd4d7ccdcd634bb500c0434f68273f3a0fb4286
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2019-10-11 23:07:39 +0530

    :)

---
+++ Find a node in LL.cpp
@@ -0,0 +1,70 @@
+/*
+Find a node in LL (recursive)
+Send Feedback
+Given a linked list and an integer n you need to find and return index where n is present in the LL. Do this recursively.
+Return -1 if n is not present in the LL.
+Indexing of nodes starts from 0.
+Input format :
+Line 1 : Linked list elements (separated by space and terminated by -1)
+Line 2 : Integer n 
+Output format :
+Index
+Sample Input 1 :
+3 4 5 2 6 1 9 -1
+5
+Sample Output 1 :
+2
+Sample Input 2 :
+3 4 5 2 6 1 9 -1
+6
+Sample Output 2 :
+4
+*/
+
+
+/**********
+ * Following is the Node class that is already written.
+ 
+class Node{
+public:
+    int data;
+    Node *next;
+    Node(int data){
+        this -> data = data;
+        this -> next = NULL;
+    }
+};
+ 
+*********/
+
+int helper(Node *head, int n, int index){
+     
+    if(head==NULL){
+        return -1;
+    }
+    
+    
+    if(head->data==n){
+        return index;
+    }
+    int smallAns=helper(head->next, n, index+1);
+    return smallAns;
+    
+    
+}
+
+
+int indexOfNRecursive(Node *head, int n) {
+    /* Don't write main().
+     * Don't read input, it is passed as function argument.
+     * Return output and don't print it.
+     * Taking input is handled automatically.
+     */
+    
+    int y=helper(head, n, 0);
+    return y;
+   
+    
+}
+
+
