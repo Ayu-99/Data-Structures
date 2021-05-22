85eb2d1382faa55480cd6e3b95c52c0e4b9e96e7
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2021-05-23 01:16:17 +0530

    Create  Chocolate Distribution Problem.py

---
+++ Greedy/Chocolate Distribution Problem.py
@@ -0,0 +1,13 @@
+def findMinDiff(self, A,N,M):
+
+        # code here
+        i=0
+        j=M-1
+        d=float("inf")
+        A.sort()
+        while j<N:
+            d=min(d,A[j]-A[i])
+            j+=1
+            i+=1
+        
+        return d 
