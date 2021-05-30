3d990351a71f84831ace031996db8972205f0d8d
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2021-05-30 05:57:12 +0530

    :)

--- Recursion/Print all Permutations.cpp
+++ Recursion/Print all Permutations.cpp
@@ -1,31 +1,24 @@
-public class solution {
+#include<bits/stdc++.h>
+using namespace std;
 
-	
-    public static void printPermutn(String str, String ans){
-        if (str.length() == 0) { 
-            System.out.println(ans); 
-            return; 
-        } 
-  
-        for (int i = 0; i < str.length(); i++) { 
-  
-            // ith character of str 
-            char ch = str.charAt(i); 
-  
-            // Rest of the string after excluding  
-            // the ith character 
-            String ros = str.substring(0, i) +  
-                         str.substring(i + 1); 
-  
-            // Recurvise call 
-            printPermutn(ros, ans + ch); 
-        } 
+void printPermutations(string s,string output){
+    if(s==""){ //base case
+        cout<<output<<endl;
+        return;
     }
-    public static void permutations(String str){
-		// Write your code here
-        String ans="";
-        printPermutn(str, ans);
-         
-
-	}
+    
+    for(int i=0;i<s.length();i++){
+        printPermutations(s.substr(0,i)+s.substr(i+1), output+s[i]);
+        
+    }
+    
 }
+
+int main(){
+    string s="cba";
+    string output="";
+    printPermutations(s, output);
+    return 0;
+}
+
+//Time complexity n!
