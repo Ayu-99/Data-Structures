22f2aedc99ada2b9dd28ae640d58f209a81bd87c
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2021-06-19 19:19:44 +0530

    :)

---
+++ Leetcode Challenge/June/K Inverse Pairs Array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int dp[1001][1001] = {};
+    int kInversePairs(int n, int k) {
+        int dp[1001][1001] = { 1 };
+        for (int N = 1; N <= n; ++N)
+            for (int K = 0; K <= k; q min(K, N - 1); ++i) 
+                    dp[N][K] = (dp[N][K] + dp[N - 1][K - i]) % 1000000007;
+        return dp[n][k];
+    }
+    // int kInversePairs(int n, int k) {
+    //     if (k <= 0)
+    //         return k == 0;
+    //     if (dp[n][k] == 0) {
+    //         dp[n][k] = 1;
+    //         for (auto i = 0; i < n; ++i) {
+    //             dp[n][k] = (dp[n][k] + kInversePairs(n - 1, k - i)) % 1000000007;
+    //         }
+    //     }
+    //     return dp[n][k] - 1;
+    // }
+};
