c4d4ed6d9d79f876677a10cf4e3f8ff4ed3a8040
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2021-06-30 16:45:12 +0530

    :)

---
+++ Leetcode Challenge/June/Python/Lowest Common Ancestor of a Binary Tree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    
+        
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root == None or root == p or root == q:
+            return root
+        
+        l = self.lowestCommonAncestor(root.left, p, q)
+		
+        r = self.lowestCommonAncestor(root.right, p, q)
+        
+        if l and r:
+            return root
+        
+        return l if l else r
