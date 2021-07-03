0c56a1008aa0a0a0894317ff182763b46621abab
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2021-07-03 17:41:14 +0530

    :)

--- Leetcode July Challenge/C++/Find K Closest Elements.cpp
+++ Leetcode July Challenge/C++/Find K Closest Elements.cpp
@@ -1,3 +1,23 @@
+// Two Pointer Approach
+/*
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int left = 0, right = arr.size()-1;
+        
+        while (right - left + 1 > k) {
+            
+            if (arr[right] - x < x - arr[left]) left++;
+            else right--;
+        }
+        
+        return vector<int>(arr.begin() + left, arr.begin() + right + 1);
+        
+    }
+};
+*/
+
+// Binary Search Approach
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
