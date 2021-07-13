afa2d0edf8fb89b055bbe7c58646c75a1751f51a
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2021-07-13 13:45:33 +0530

    :)

---
+++ Leetcode July Challenge/C++/Find Peak Element.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& arr) {
+        
+        int n = arr.size();
+        if(n == 1){
+            return 0;
+        }
+        int low = 0, high = n - 1;
+        while (low <= high){
+            
+            int mid = low + (high - low)/2;
+            if(mid == 0){
+                if(arr[mid] > arr[mid + 1]){
+                    return mid;
+                }else{
+                    low = mid + 1;
+                }
+            
+            }else if(mid == n - 1){
+                if(arr[mid - 1] < arr[mid]){
+                    return mid;    
+                }else{
+                    high = mid - 1;
+                }
+                 
+            }else{
+                if(arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]){
+                    return mid;
+                }
+                else if(arr[mid] < arr[mid + 1]){
+                    low = mid + 1;
+                }else{
+                    high = mid - 1;
+                }
+            }
+            
+        }
+        
+        return -1;
+    }
+};
