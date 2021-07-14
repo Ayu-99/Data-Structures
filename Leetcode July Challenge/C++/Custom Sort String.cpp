65d076781515dbf16847188903e9481172675f1a
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2021-07-14 17:39:52 +0530

    :)

---
+++ Leetcode July Challenge/C++/Custom Sort String.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string customSortString(string order, string str) {
+        string s1, s2;
+        int a[26]={0};
+        for(char c: order) a[c-'a']++;
+        for(char c: str){
+            if(a[c-'a']==0) s2+=c;
+            else a[c-'a']++;
+        }
+        for(char c: order){
+            while(a[c-'a']>1){
+                s1+=c; 
+                a[c-'a']--;
+            } 
+        }
+        return s1+s2;
+    }
+};
