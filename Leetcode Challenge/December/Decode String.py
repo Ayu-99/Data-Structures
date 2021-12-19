c53ac133c7865cb243974b89929c1b7b3f8cc427
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2021-12-19 18:25:35 +0530

    Create Decode String.py

---
+++ Leetcode Challenge/December/Decode String.py
@@ -0,0 +1,19 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []; curNum = 0; curString = ''
+        for c in s:
+            if c == '[':
+                stack.append(curString)
+                stack.append(curNum)
+                curString = ''
+                curNum = 0
+            elif c == ']':
+                num = stack.pop()
+                prevString = stack.pop()
+                curString = prevString + num*curString
+            elif c.isdigit():
+                curNum = curNum*10 + int(c)
+            else:
+                curString += c
+        return curString 
+        
