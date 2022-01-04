6f2306572435dcb6373340f869caa6f0e964b305
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-01-04 19:16:56 +0530

    Update Number Complement.cpp

--- Leetcode Challenge/December/Number Complement.cpp
+++ Leetcode Challenge/December/Number Complement.cpp
@@ -2,7 +2,11 @@ class Solution {
 public:
     
     int findComplement(long num) {
+        if(num==0) {
+            return 1;
+        }
         for(long i=1;i<=num;i*=2) num^=i; 
         return num;
+    
     }
 };
