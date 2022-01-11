0b0acecd69f44ce3f687be25b4c3d4c22d54bda7
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-01-11 16:29:50 +0530

    Create Sum of Root To Leaf Binary Numbers.cpp

---
+++ Leetcode Challenge/January/Sum of Root To Leaf Binary Numbers.cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumRootToLeaf(TreeNode* root, int val = 0) {
+        if (!root) return 0;
+        val = (val * 2 + root->val);
+        return root->left == root->right ? val : sumRootToLeaf(root->left, val) + sumRootToLeaf(root->right, val));
+    }
+};
