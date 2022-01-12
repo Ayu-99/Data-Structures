7b8724fd3948abd8ad08d5bbd9efc818ffed9bf2
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-01-12 13:50:11 +0530

    Update Sum of Root To Leaf Binary Numbers.cpp

--- Leetcode Challenge/January/Sum of Root To Leaf Binary Numbers.cpp
+++ Leetcode Challenge/January/Sum of Root To Leaf Binary Numbers.cpp
@@ -14,6 +14,6 @@ public:
     int sumRootToLeaf(TreeNode* root, int val = 0) {
         if (!root) return 0;
         val = (val * 2 + root->val);
-        return root->left == root->right ? val : sumRootToLeaf(root->left, val) + sumRootToLeaf(root->right, val));
+        return root->left == root->right ? val : sumRootToLeaf(root->left, val) + sumRootToLeaf(root->right, val);
     }
 };
