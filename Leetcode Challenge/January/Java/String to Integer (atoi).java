46407b7a16fac2b97c63c4679022877c0cbb0e2c
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-01-14 17:50:39 +0530

    Create String to Integer (atoi).java

---
+++ Leetcode Challenge/January/Java/String to Integer (atoi).java
@@ -0,0 +1,30 @@
+class Solution {
+    public int myAtoi(String s) {
+        if(s==null) return 0;
+
+        s=s.trim();
+        
+        if(s.length()==0) return 0;
+        
+        int sign = +1;
+        long ans = 0;
+        if(s.charAt(0) == '-') sign = -1;
+        
+        int MAX = Integer.MAX_VALUE, MIN = Integer.MIN_VALUE;
+		
+		// initiate the starting pointer
+        int i = (s.charAt(0) == '+' || s.charAt(0) == '-') ? 1 : 0;
+		
+        while(i < s.length()) {
+            if(s.charAt(i) == ' ' || !Character.isDigit(s.charAt(i))) break;
+            ans = ans * 10 + s.charAt(i)-'0';
+            // check the conditions
+            if(sign == -1 && -1*ans < MIN) return MIN;
+            if(sign == 1 && ans > MAX) return MAX;
+            
+            i++;
+        }
+        
+        return (int)(sign*ans);
+    }
+}
