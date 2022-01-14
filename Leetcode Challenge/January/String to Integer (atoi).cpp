bb969c7535604bad42c55b1ddc3b460b2e9380b5
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-01-14 17:50:14 +0530

    Create String to Integer (atoi).cpp

---
+++ Leetcode Challenge/January/String to Integer (atoi).cpp
@@ -0,0 +1,32 @@
+class Solution {
+    public :
+    int myAtoi(string s) {
+        if(s.length()==0) return 0;
+
+        int i=0;
+         while(i<s.size() && s[i]== ' ') {
+             i++;
+         }     
+        s = s.substr(i); //i ---> last of string
+        
+        int sign = +1;
+        long ans = 0;
+        
+        if(s[0] == '-') sign = -1;
+        
+        int MAX = INT_MAX, MIN = INT_MIN;
+        i = (s[0] == '+' || s[0] == '-') ? 1 : 0;
+		
+        while(i < s.length()) {
+            if(s[0] == ' ' || !isdigit(s[i])) break;
+            
+            ans = ans * 10 + s[i]-'0';
+            if(sign == -1 && -1*ans < MIN) return MIN;
+            if(sign == 1 && ans > MAX) return MAX;
+            
+            i++;
+        }q
+        
+        return (int)(sign*ans);
+    }
+};
