3f7a9a1e605060f3410544fb675c8931010e56ef
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-01-15 18:18:01 +0530

    Create Jump Game IV.java

---
+++ Leetcode Challenge/January/Java/Jump Game IV.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minJumps(int[] arr) {
+        int n = arr.length;
+        HashMap<Integer, List<Integer>> indicesOfValue = new HashMap<>();
+        for (int i = 0; i < n; i++)
+            indicesOfValue.computeIfAbsent(arr[i], x -> new LinkedList<>()).add(i);
+        boolean[] visited = new boolean[n]; visited[0] = true;
+        Queue<Integer> q = new LinkedList<>(); q.offer(0);
+        int step = 0;
+        while (!q.isEmpty()) {
+            for (int size = q.size(); size > 0; --size) {
+                int i = q.poll();
+                if (i == n - 1) return step; // Reached to last index
+                List<Integer> next = indicesOfValue.get(arr[i]);
+                next.add(i - 1); next.add(i + 1);
+                for (int j : next) {
+                    if (j >= 0 && j < n && !visited[j]) {
+                        visited[j] = true;
+                        q.offer(j);
+                    }
+                }
+                indicesOfValue.get(arr[i]).clear(); // avoid later lookup indicesOfValue arr[i]
+            }
+            step++;
+        }
+        return 0;
+    }
+}
