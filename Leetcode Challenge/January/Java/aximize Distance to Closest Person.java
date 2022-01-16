8327db83d434eb71826734358c7e3f27569ad90a
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-01-16 18:19:03 +0530

    Create aximize Distance to Closest Person.java

---
+++ Leetcode Challenge/January/Java/aximize Distance to Closest Person.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxDistToClosest(int[] seats) {
+        int pre_zeros = -1, suf_zeros = -1, max_zeros = -1, zeros = 0;
+        for (int i = 0; i < seats.length; i ++) {
+            if (seats[i] == 0) {
+                zeros ++;
+            } else {
+                if (pre_zeros == -1) {
+                    pre_zeros = zeros;
+                } else {
+                    max_zeros = Math.max(max_zeros, zeros);
+                }
+                zeros = 0;
+            }
+        } 
+        suf_zeros = zeros;
+        return Math.max(Math.max(pre_zeros, suf_zeros), (max_zeros + 1) / 2);
+    }
+}
