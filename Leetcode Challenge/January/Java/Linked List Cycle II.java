7d0d584bd39876ab6869b426b05a0c6ca7d094b1
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-01-19 15:36:31 +0530

    Create Linked List Cycle II.java

---
+++ Leetcode Challenge/January/Java/Linked List Cycle II.java
@@ -0,0 +1,15 @@
+public ListNode detectCycle(ListNode head) {
+	ListNode slow = head, fast = head;
+	while(fast != null && fast.next != null) {
+		fast = fast.next.next;
+		slow = slow.next;
+		if (slow == fast) {
+			while (head != slow) {
+				head = head.next;
+				slow = slow.next;
+			}
+			return slow;				
+		}
+	}			
+	return null;
+}
