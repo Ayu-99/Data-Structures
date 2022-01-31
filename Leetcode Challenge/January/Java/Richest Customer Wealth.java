d0665b32a746dd0be4a898c3074b4a0edc08ce6a
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-01-31 13:49:07 +0530

    Create Richest Customer Wealth.java

---
+++ Leetcode Challenge/January/Java/Richest Customer Wealth.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int maxWealth = 0;
+        int rows = accounts.length;
+        int cols = accounts[0].length;
+        for(int i = 0; i < rows; i++) {
+            int wealth = 0;
+            for(int j = 0; j < cols; j++) {
+                wealth += accounts[i][j];
+            }
+            
+            maxWealth = Math.max(maxWealth, wealth);
+        }
+        return maxWealth;
+    }
+}
