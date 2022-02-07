b653a055c9eee49fa0c7acf55f01116ef50e1230
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-02-07 13:20:47 +0530

    Create Find the Difference.java

---
+++ Leetcode Challenge/February/java/Find the Difference.java
@@ -0,0 +1,14 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+                
+	char c = 0;
+	for (int i = 0; i < s.length(); ++i) {
+		c ^= s.charAt(i);
+	}
+	for (int i = 0; i < t.length(); ++i) {
+		c ^= t.charAt(i);
+	}
+	return c;
+
+    }
+}
