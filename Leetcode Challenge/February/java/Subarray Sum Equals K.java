959416ed27c05cad00354d721b51c008270cdd95
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-02-10 17:56:35 +0530

    Create Subarray Sum Equals K.java

---
+++ Leetcode Challenge/February/java/Subarray Sum Equals K.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0, sum = 0;
+        HashMap < Integer, Integer > map = new HashMap < > ();
+        
+      
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if(sum == k) count++;
+            if (map.containsKey(sum - k))
+                count += map.get(sum - k);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+}
