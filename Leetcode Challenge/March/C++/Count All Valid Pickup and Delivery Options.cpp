b463480b1ec6db1f5cab7efda4ac73c92e669d71
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-03-06 20:28:40 +0530

    Create Count All Valid Pickup and Delivery Options.cpp

---
+++ Leetcode Challenge/March/C++/Count All Valid Pickup and Delivery Options.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countOrders(int n) {
+        long res = 1, mod = 1000000007;
+        for(int i = 1; i <=n; ++i)
+        {
+            res *= i;
+            res %= mod;
+            res *= (2*i-1);
+            res %= mod;
+        }
+        
+        return res % mod;
+    }
+};
