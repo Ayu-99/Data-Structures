eb738149dfdf1bdc5ddd35a97404bd69d20f764a
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-03-23 23:26:06 +0530

    Create Broken Calculator.cpp

---
+++ Leetcode Challenge/March/C++/Broken Calculator.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+        if(startValue >= target) return startValue - target;
+        if(target % 2 == 0){ //TARGET IS EVEN
+            return 1 + brokenCalc(startValue, target / 2);
+        }
+        return 1 + brokenCalc(startValue, target + 1);     
+    }
+};
