fb8eeb76e814916c7e01a42728a41aa005737361
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-04-04 09:30:43 +0530

    Create Swapping Nodes in a Linked List.cpp

---
+++ Leetcode Challenge/April/C++/Swapping Nodes in a Linked List.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapNodes(ListNode* head, int k) {
+        ListNode *ptr1 = head, *ptr2 = head, *kth = NULL;
+        while (k--){
+            ptr1 = ptr1->next;
+        }
+            
+        
+        kth = ptr1; //kth node from start
+        ptr1 = ptr1->next;
+        
+        while (ptr1 != NULL) {
+            ptr1 = ptr1->next;
+            ptr2 = ptr2->next;
+        }
+        
+        swap(ptr2->val, kth->val);
+        return head;
+    }
+};
