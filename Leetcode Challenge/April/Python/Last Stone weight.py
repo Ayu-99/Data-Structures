cded48a3b99bd5925929aad83d71e21c969417c6
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-04-07 22:13:49 +0530

    Create Last Stone weight.py

---
+++ Leetcode Challenge/April/Python/Last Stone weight.py
@@ -0,0 +1,6 @@
+ def lastStoneWeight(self, A):
+        h = [-x for x in A]
+        heapq.heapify(h)
+        while len(h) > 1 and h[0] != 0:
+            heapq.heappush(h, heapq.heappop(h) - heapq.heappop(h))
+        return -h[0]
