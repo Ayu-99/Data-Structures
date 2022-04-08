bc6ccc66762da42f01b138a80e555264c0a25076
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-04-08 10:24:15 +0530

    Create Kth Largest Element in a Stream.java

---
+++ Leetcode Challenge/April/Java/Kth Largest Element in a Stream.java
@@ -0,0 +1,21 @@
+class KthLargest {
+        final PriorityQueue<Integer> q;
+        final int k;
+
+        public KthLargest(int k, int[] a) {
+            this.k = k;
+            q = new PriorityQueue<>(k);
+            for (int n : a)
+                add(n);
+        }
+
+        public int add(int n) {
+            if (q.size() < k)
+                q.offer(n);
+            else if (q.peek() < n) {
+                q.poll();
+                q.offer(n);
+            }
+            return q.peek();
+        }
+    }
