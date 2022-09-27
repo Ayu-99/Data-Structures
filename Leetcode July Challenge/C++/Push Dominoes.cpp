fecbc320ead96448b743d08f2d89694c503ecd8d
Anshuman behera <90245492+anshumanbehera27@users.noreply.github.com>
2022-09-27 19:28:28 +0530

    This approach will get TLE  In java
    I was trying multiple times with your  approach in java but It will show me the TLE . so I will come up with this approach   now this  code will work fine in Leetcode 
    And thank you for your Video it"s beneficial while solving this problem.

--- Leetcode July Challenge/C++/Push Dominoes.cpp
+++ Leetcode July Challenge/C++/Push Dominoes.cpp
@@ -71,3 +71,58 @@ public:
         return finalResult;
     }
 };
+
+// java code for this solution 
+class Solution {
+        public String pushDominoes(String dominoes) {
+        int n = dominoes.length();
+        char[] s = dominoes.toCharArray();
+        char start = '.',end = '.';
+        int startIdx = 0,i=0;
+        
+        while(i<n){
+            startIdx =i;
+            while(i<n && s[i] == '.')
+                i++;
+            if(i<n)
+                end = s[i];
+            if(i-startIdx>0){
+                if(start == 'R' && end == 'L'){
+                    int l = startIdx;
+                    int r = i-1;
+                    while(l<r){
+                        s[l] = 'R';
+                        s[r] = 'L';
+                        l++;
+                        r--;
+                    }
+                }
+                else if(end == 'L'){
+                    int l = i-1;
+                    while(l>=startIdx){
+                        s[l]= 'L';
+                        l--;
+                    }
+                }
+                else if(start == 'R'){
+                    int r = startIdx;
+                    while(r<i){
+                        s[r] = 'R';
+                        r++;
+                    }
+                }
+            }
+            start = end;
+            i++;
+            end = '.';
+        }
+        return new String(s);
+    }
+
+
+
+
+
+
+
+
