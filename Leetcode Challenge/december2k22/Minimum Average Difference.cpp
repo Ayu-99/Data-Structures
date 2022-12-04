c94c30452e6a3dbe74a3576bfd54c94bd65af672
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-12-04 21:15:32 +0530

    Create Minimum Average Difference.cpp

---
+++ Leetcode Challenge/december2k22/Minimum Average Difference.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minimumAverageDifference(vector<int>& nums) {
+        
+        int n(size(nums)), minAverageDifference(INT_MAX), index;
+        
+        long long sumFromFront(0), sumFromEnd(0);
+        for (auto& num : nums) sumFromEnd += num;
+        
+        for (int i=0; i<n; i++) {
+            sumFromFront += nums[i];
+            sumFromEnd -= nums[i];
+            int a = sumFromFront / (i+1); 
+            int b = (i == n-1) ? 0 : sumFromEnd / (n-i-1); 
+            if (abs(a-b) < minAverageDifference) {
+                minAverageDifference = abs(a-b);
+                index = i;
+            }
+        }
+        return index;
+    }
+};
