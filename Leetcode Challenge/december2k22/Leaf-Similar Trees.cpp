510d56459fe7851faa5f0be176ea10793442b757
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2022-12-08 19:09:23 +0530

    Create Leaf-Similar Trees.cpp

---
+++ Leetcode Challenge/december2k22/Leaf-Similar Trees.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        sq
+        DFS(root1, t1);
+        DFS(root2, t2);
+        return t1==t2;
+    }
+    
+    void DFS(TreeNode* root, string& s) {
+        if(root==NULL) return;
+        if(root->left==NULL&&root->right==NULL) s+=to_string(root->val)+"*";
+        DFS(root->left, s);
+        DFS(root->right, s);
+    }
+};
