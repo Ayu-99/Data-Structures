ac026c36f906dbb58ca389fd9ad36370ba395127
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2024-08-16 14:51:01 +0530

    Create Maximum Distance in Arrays.py

---
+++ Greedy/Python/Maximum Distance in Arrays.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def maxDistance(self, arrays):
+        smallest = arrays[0][0]
+        biggest = arrays[0][-1]
+        max_distance = 0
+
+        for i in range(1, len(arrays)):
+            arr = arrays[i]
+            max_distance = max(max_distance, abs(arr[-1] - smallest), abs(biggest - arr[0]))
+            smallest = min(smallest, arr[0])
+            biggest = max(biggest, arr[-1])
+
+        return max_distance
