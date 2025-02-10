23571a07304e5ae5f2ff212d6db0f9d2640d2687
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2025-02-10 21:50:38 +0530

    Create Clear digits.java

---
+++ Leetcode Feb challenge2k25/Clear digits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String clearDigits(String s) {
+        StringBuilder ans= new StringBuilder();
+        for(char c: s.toCharArray()) {
+            if(Character.isDigit(c)) {
+                if (ans.length() > 0) {
+                    ans.deleteCharAt(ans.length()-1);
+                } 
+            } else {
+                ans.append(c);
+            }
+        }
+        return ans.toString();
+    }
+}
+}
