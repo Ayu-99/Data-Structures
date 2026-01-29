4d22928c6fc293032d474dfff1a45fb16eb37952
Ayushi Sharma <51323447+Ayu-99@users.noreply.github.com>
2026-01-29 19:40:32 +0530

    Implement sortedSquares method to square and sort array

---
+++ Squares of a Sorted Array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        int left = 0, right = n - 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+                result[i] = nums[left] * nums[left];
+                left++;
+            } else {
+                result[i] = nums[right] * nums[right];
+                right--;
+            }
+        }
+        return result;
+    }
+}
